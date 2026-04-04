c3926278e61cd158cc187c1fe36464eb3c7558af
kismatdahal24@gmail.com <kismatdahal24@gmail.com>
2026-04-04 21:31:34 +0545

    basic program

---
+++ Desktop/bsc-csit-first-sem-c-programs-2-main/bsc-csit-first-sem-c-programs-2-main/basic/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter any two numbers");
+    scanf("%d %d",&a,&b);
+    sum=a +b;
+    printf("the sum is %d",sum);
+    return 0;
+
+}
