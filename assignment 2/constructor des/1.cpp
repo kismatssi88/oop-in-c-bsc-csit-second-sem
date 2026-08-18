b6b69b1f58c1e7f32b211bdc43e6fdd641f183f5
kismatdahal24@gmail.com <kismatdahal24@gmail.com>
2026-08-18 20:36:43 +0545

    oop

--- assignment 2/constructor des/1.cpp
+++ assignment 2/constructor des/1.cpp
@@ -1,35 +1,27 @@
-// 1. Constructor and Destructor (Basic Implementation)
-// Write a C++ program to create a class Person with:
-//  Data members: name and age.
-//  A constructor to initialize these values.
-//  A destructor to display a message when an object is destroyed.
-// Demonstrate object creation and automatic destructor call at the end of the program.
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Person {
-    private:
-        string name;
-        int age;
-    
-    public:
-        Person(string n, int a) {
-            name = n;
-            age = a;
-        }
+class Student {
+private:
+    string name;
+    int age;
+public:
+    // Constructor
+    Student() {
+        name = "Kismat";
+        age = 20;
+    }
 
-        void display() {
-            cout <<"Name: "<< name <<" "<< "Age: "<< age<< endl;
-        }
-
-        ~Person() {
-            cout << "Destructor called";
-        }
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
 };
 
 int main() {
-    Person p("kessi", 19);
-    p.display();
-    return 0;
+    Student s1;   // Constructor is automatically called
+
+    s1.display();
+
+
 }
